422ac52d1739c2f7599d687527b24a7ea245fd6a
MrBSD <MrBSD82@gmail.com>
2017-08-23 16:49:22 +0300

    Added DicountRate to Customer view

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,28 +24,20 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers
+                .Include(c=>c.MembershipType)
+                .ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
             {
                 return HttpNotFound();
             }
             return View(customer);
-        }
-
-        public IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer() {Id = 1, Name = "John Smith"},
-                new Customer() {Id = 2, Name = "Forrest Gump"}
-            };
-
         }
     }
 }
