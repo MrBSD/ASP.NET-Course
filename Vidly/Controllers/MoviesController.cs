d4f479b84a85c439c7ed70eb9ac1c34b85d8be53
MrBSD <MrBSD82@gmail.com>
2017-08-23 14:53:42 +0300

    CustomerController added. Exercises

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -35,23 +35,25 @@ namespace Vidly.Controllers
             return Content($"id = {id}");
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-            if (sortBy.IsNullOrWhiteSpace())
-            {
-                sortBy = "Name";
-            }
-            return Content(string.Format($"pageIndex={pageIndex}&sortBy={sortBy}"));
+            var movies = GetMovies();
+            return View(movies);
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content($"{year}//{month}");
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie() {Id = 1, Name = "Shrek"},
+                new Movie() {Id = 2, Name = "Wall-e"}
+            };
         }
     }
 }
