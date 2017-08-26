dfe93668f8d525e87f9fd54001c4258bc5c22763
MrBSD <MrBSD82@gmail.com>
2017-08-26 09:38:16 +0300

    added Custom validation (Name and Age)

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,9 +9,12 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Name is required")]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Min18YearsIfAMAmber]
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedForNewsleters { get; set; }
         public MembershipType MembershipType { get; set; }
